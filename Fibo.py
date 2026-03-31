d1cc09f20d99471d80c440745743ffaaaec5dbfe
Ujwal Sai Meduri <umeduri@eu.trimblecorp.net>
2026-03-31 15:11:56 +0530

    just all

--- Fibo.py
+++ Fibo.py
@@ -3,7 +3,7 @@ def fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
         series.append(a)
-        print("now finished")
+        print("now finished all")
         print("hello2")
     return series
 
