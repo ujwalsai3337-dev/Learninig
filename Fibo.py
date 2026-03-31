06fd8ec8cc4970b7113105755bf3f84bb251954e
ujwalsai3337-dev <ujwalsai3337@gmail.com>
2026-03-31 13:10:05 +0530

    Update Fibo.py

--- Fibo.py
+++ Fibo.py
@@ -3,7 +3,7 @@ def fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
         series.append(a)
-        print("got it now at  this time")
+        print("changed")
     return series
 
 print(fibonacci(10))
