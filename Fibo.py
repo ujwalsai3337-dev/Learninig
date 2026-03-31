b19e0ad0188d7c233e25c6a86fd5e32d8bf80996
Ujwal Sai Meduri <umeduri@eu.trimblecorp.net>
2026-03-31 12:55:23 +0530

    got it

--- Fibo.py
+++ Fibo.py
@@ -3,8 +3,7 @@ def fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
         series.append(a)
-        # print("change4")
-        # print(hi)
+        print("got it")
     return series
 
 print(fibonacci(10))
